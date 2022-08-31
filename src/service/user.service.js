7a16310915e9b4e571fcc467f9f1d3da7d3c7662
lingronghai <lingronghai@foxmail.com>
2022-08-31 16:35:27 +0800

    新增用户创建创建与查询逻辑

---
+++ src/service/user.service.js
@@ -0,0 +1,20 @@
+const connection = require('../app/database')
+
+class UserService {
+  async create(user) {
+    const { name, password } = user
+    const statement = `INSERT INTO user (name, password) VALUES (?, ?);`
+    // MySQL 提供了 execute 辅助函数，它将准备和查询语句，可以使用 prepare / unprepare 方法手动准备/取消准备
+    const result = await connection.execute(statement, [name, password])
+
+    return result[0]
+  }
+  async getUserByName(name) {
+    const statement = `SELECT * FROM user WHERE name = ?;`
+    const result = await connection.execute(statement, [name])
+
+    return result[0]
+  }
+}
+
+module.exports = new UserService()
