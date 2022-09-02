f9c75ef39851d972d78f0b9081d8fdd093f7f6f2
lingronghai <lingronghai@foxmail.com>
2022-09-03 00:22:28 +0800

    新增密钥对以及配置

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
+const path = require('path')
 const dotenv = require('dotenv')
+
+const PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './keys/private.key'))
+const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './keys/public.key'))
 
 dotenv.config()
 
@@ -11,3 +16,6 @@ module.exports = {
   MYSQL_USER,
   MYSQL_PASSWORD
 } = process.env
+
+module.exports.PRIVATE_KEY = PRIVATE_KEY
+module.exports.PUBLIC_KEY = PUBLIC_KEY
